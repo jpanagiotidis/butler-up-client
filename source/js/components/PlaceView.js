17347535d5a57d715385f5f9fbdf40ba9d64dd58
Jonathan <panagiotidisjonathan@gmail.com>
2016-03-05 18:26:37 +0200

    added catalogues and styling in the place page

--- source/js/components/PlaceView.js
+++ source/js/components/PlaceView.js
@@ -22,15 +22,55 @@ class PlaceView extends Component{
     if(self.props.isLoading){
       content = (<h1>Loading...</h1>)
     }else if(place){
+      const catalogues = place.catalogues.map(function(cat, index){
+        return (
+          <li className="bu-section" key={index}>
+            <a href={cat.url}>
+              {cat.title}
+            </a>
+          </li>
+        );
+      });
       content = (
         <article className="bu-place">
           <h1 className="bu-place-header">{place.title}</h1>
-          <div className="bu-place-image-holder">
+          <section className="bu-place-image-holder">
             <img src={place.main_image}/>
-          </div>
-          <div className="bu-place-description">
-            {place.description}
-          </div>
+          </section>
+          <h2 className="bu-place-section-header bu-section">Περιγραφή</h2>
+          <section 
+            className="bu-place-description bu-section"
+            dangerouslySetInnerHTML={{__html:place.description}}
+          />
+          <h2 className="bu-place-section-header bu-section">Κατάλογοι</h2>
+          <section className="bu-place-catalogues">
+            <ul className="catalogues">
+              {catalogues}
+            </ul>
+          </section>
+          <h2 className="bu-place-section-header bu-section">Πληροφορίες</h2>
+          <section className="bu-place-info">
+            <table>
+              <tbody>
+                <tr>
+                  <th>
+                    Διεύθυνση:
+                  </th>
+                  <td>
+                    asdfasfads
+                  </td>
+                </tr>
+                <tr>
+                  <th>
+                    Τηλέφωνο:
+                  </th>
+                  <td>
+                    asdfasfads
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
         </article>
       );
     }
