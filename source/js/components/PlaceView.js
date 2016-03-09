d96a0d0aeeb87e3f9f8c2c58ce27a225019d4e30
Jonathan <panagiotidisjonathan@gmail.com>
2016-03-09 23:33:57 +0200

    added multiple type icons on place page

--- source/js/components/PlaceView.js
+++ source/js/components/PlaceView.js
@@ -3,6 +3,7 @@
 import React, {Component} from 'react';
 import {branch} from 'baobab-react/higher-order';
 import {getPlace} from '../actions/places.js';
+import {getTypeIcon} from '../actions/placeTypes.js';
 import {getString} from '../managers/StringsManager.js';
 import {Loader} from '../components';
 
@@ -64,6 +65,15 @@ class PlaceView extends Component{
     );
   }
 
+  getIcons(){
+    const self = this;
+    const place = self.props.places[self.props.params.placeId];
+
+    return place.type.map(function(type){
+      return (<i key={type} className={`fa fa-2x ${getTypeIcon(type)}`}/>);
+    });
+  }
+
   render(){
     const self = this;
     const place = self.props.places[self.props.params.placeId];
@@ -85,6 +95,9 @@ class PlaceView extends Component{
         <div>
           <article className="bu-place">
             <h1 className="bu-place-header bu-section">{place.title}</h1>
+            <section className="bu-place-description bu-section">
+              {self.getIcons()}
+            </section>
             <section className="bu-place-image-holder bu-section">
               <img src={place.main_image}/>
             </section>
